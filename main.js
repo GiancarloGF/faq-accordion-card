b01c7cc18d50b7e1d9a71a7c4f5d74efb66949a2
Giancarlo Guerra <gian.guerra23@gmail.com>
2021-03-11 20:59:33 -0500

    project finished faq accordion

---
+++ main.js
@@ -0,0 +1,22 @@
+
+let containerFaqs= document.querySelector('.container-faqs');
+
+let myTarget;
+
+containerFaqs.addEventListener('click', (e)=>{
+    myTarget=e.target;
+    if (myTarget.classList[0]!='container-faqs'  && myTarget.classList[0]!='faqs') {//aseguramos: Que el elemento del evento solo sea <p> o <i>.
+        myTarget.parentElement.parentElement.children[1].classList.toggle('active')
+        myTarget.parentElement.children[1].classList.toggle('fa-angle-down');
+        myTarget.parentElement.children[1].classList.toggle('fa-angle-up');
+        
+    } else {
+        return;
+    }
+})
+
+
+
+
+
+myTarget.parentElement.parentElement.children[1].classList.toggle('active')
