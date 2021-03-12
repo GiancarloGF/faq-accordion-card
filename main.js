088c7bea7eb7ccb7a848da72e1688928e3a284de
Giancarlo Guerra <gian.guerra23@gmail.com>
2021-03-11 21:19:28 -0500

    Arreglando 3er bug (solo click en p e i)

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let myTarget;
 
 containerFaqs.addEventListener('click', (e)=>{
     myTarget=e.target;
-    if (myTarget.classList[0]!='container-faqs'  && myTarget.classList[0]!='faqs') {//aseguramos: Que el elemento del evento solo sea <p> o <i>.
+    if (myTarget.classList[0]!='container-faqs'  && myTarget.classList[0]!='faqs' && myTarget.classList[0]!='faqs-top' ) {//aseguramos: Que el elemento del evento solo sea <p> o <i>.
         myTarget.parentElement.parentElement.children[1].classList.toggle('active')
         myTarget.parentElement.children[1].classList.toggle('fa-angle-down');
         myTarget.parentElement.children[1].classList.toggle('fa-angle-up');
